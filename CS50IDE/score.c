4aab671a9ef754872e22582791660a18f286798c
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-09-05 17:36:18 +0900

    Add files via upload
    [부스트코스] 모두를 위한 컴퓨터 과학 (CS50 2019)
    Ch3. 배열 5)배열(2)

--- CS50IDE/score.c
+++ CS50IDE/score.c
@@ -1,23 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//  평균 계산 함수 : 배열의 길이와 배열을 입력으로 받는다
+//                   함수 안에서 배열의 길이만큼 루프를 돈다
+float average (int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return (float) sum / (float) length;
+}
+
 int main(void)
 {
-    
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    //  사용자로부터 점수 입력 수를 입력
+    int n = get_int("Number of scores: ");
 
-    printf("Average : %i\n", (score1 + score2 + score3) / 3);
+    //  점수 배열 선언 및 사용자로부터 값 입력
+    int scores[n];
 
-    // 위의 코딩을 좀더 개선된 디자인으로 표현
-    int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Score %i: ", i+1);
+    }
 
-    printf("Average[] : %i\n", (scores[0] + scores[1] + scores[2]) / 3);
-    
-    
-
+    //  평균 출력
+    printf("Average : %.1f\n", average(n, scores));
 }
+
