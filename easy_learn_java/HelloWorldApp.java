6c10356f17c43baf6c377925a4f033dd8185bc14
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-09-06 21:49:45 +0900

    second commit and update test via web upload
    add txt; "github, file upload and file log update check via site upload"

--- easy_learn_java/HelloWorldApp.java
+++ easy_learn_java/HelloWorldApp.java
@@ -1,6 +1,6 @@
 public class HelloWorldApp {
     public static void main(String args[]) {
         System.out.println("Hello, World");
-      
+        System.out.println("github, file upload and file log update check via site upload");
     }
 }
